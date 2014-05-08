56b5c38cf1ed247c455609e7485b6899837fbc8b
dlavoie <dlavoie@cspinformatique.com>
2014-05-08 18:50:14 +0200

    wip

--- src/main/java/com/cspinformatique/wevan/google/GoogleCalendar.java
+++ src/main/java/com/cspinformatique/wevan/google/GoogleCalendar.java
@@ -1,95 +1,92 @@
 package com.cspinformatique.wevan.google;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.TimeZone;
+
+import org.apache.commons.io.IOUtils;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
 
 public class GoogleCalendar {
-	private static final String APPLICATION_NAME = "wevan";
+	private static final String APPLICATION_NAME = "My Project";
+
+	/** E-mail address of the service account. */
+	private static final String SERVICE_ACCOUNT_EMAIL = "141051216875-o6d4m2qsl7n3od6kj1klc0cu6gsf2c7r@developer.gserviceaccount.com";
 	
 	private static final JsonFactory JSON_FACTORY = JacksonFactory
 			.getDefaultInstance();
 
-	private static final java.io.File DATA_STORE_DIR = new java.io.File(
-			System.getProperty("user.home"), ".store/calendar_sample");
-
 	private static Calendar client;
-	private static FileDataStoreFactory dataStoreFactory;
 	private static HttpTransport httpTransport;
 
 	private static Credential authorize() throws Exception {
-		// load client secrets
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets
-				.load(JSON_FACTORY,
-						new InputStreamReader(
-								GoogleCalendar.class
-										.getResourceAsStream("/google/calendar/client_secrets.json")));
-		if (clientSecrets.getDetails().getClientId().startsWith("Enter")
-				|| clientSecrets.getDetails().getClientSecret()
-						.startsWith("Enter ")) {
-			System.out
-					.println("Overwrite the src/main/resources/client_secrets.json file with the client secrets file "
-							+ "you downloaded from the Quickstart tool or manually enter your Client ID and Secret "
-							+ "from https://code.google.com/apis/console/?api=calendar#project:141051216875 "
-							+ "into src/main/resources/client_secrets.json");
-			System.exit(1);
-		}
-
-		// Set up authorization code flow.
-		// Ask for only the permissions you need. Asking for more permissions
-		// will
-		// reduce the number of users who finish the process for giving you
-		// access
-		// to their accounts. It will also increase the amount of effort you
-		// will
-		// have to spend explaining to users what you are doing with their data.
-		// Here we are listing all of the available scopes. You should remove
-		// scopes
-		// that you are not actually using.
-		Set<String> scopes = new HashSet<String>();
-		scopes.add(CalendarScopes.CALENDAR);
-		scopes.add(CalendarScopes.CALENDAR_READONLY);
-
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-				httpTransport, JSON_FACTORY, clientSecrets, scopes).setApprovalPrompt("auto").build();
-
-		// authorize
-		return new AuthorizationCodeInstalledApp(flow,
-				new LocalServerReceiver()).authorize("user");
+        httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        // check for valid setup
+        if (SERVICE_ACCOUNT_EMAIL.startsWith("Enter ")) {
+          System.err.println(SERVICE_ACCOUNT_EMAIL);
+          System.exit(1);
+        }
+        
+        String p12Content = IOUtils.toString(
+        		GoogleCalendar.class.getClassLoader().getResource(
+            			"google/calendar/calendar.p12").openStream());
+        
+        if (p12Content.startsWith("Please")) {
+        	throw new RuntimeException("Invalid private key file content : " + p12Content);
+        }
+        
+		Set<String> scopes = CalendarScopes.all();
+		
+        // service account credential (uncomment setServiceAccountUser for domain-wide delegation)
+        return new GoogleCredential.Builder().setTransport(httpTransport)
+            .setJsonFactory(JSON_FACTORY)
+            .setServiceAccountId(SERVICE_ACCOUNT_EMAIL)
+            .setServiceAccountScopes(scopes)
+            .setServiceAccountPrivateKeyFromP12File(
+            	new File(GoogleCalendar.class.getClassLoader().getResource("google/calendar/calendar.p12").getFile())
+            )
+            // .setServiceAccountUser("user@example.com")
+            .build();
 	}
 
 	public GoogleCalendar() {
 		try {
-			httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-
-			// initialize the data store factory
-			dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
-
 			// authorization
 			Credential credential = authorize();
 
-			// set up global Calendar instance
-			client = new Calendar.Builder(httpTransport, JSON_FACTORY,
-					credential).setApplicationName(APPLICATION_NAME).build();
+			Calendar calendar = new Calendar.Builder(httpTransport, JSON_FACTORY, credential)
+            .setApplicationName(APPLICATION_NAME).build();
 			
-			System.out.println(credential.getAccessToken());
-
-			System.out.println("Success! Now add code here.");
+			com.google.api.services.calendar.model.Calendar carCalendar = new com.google.api.services.calendar.model.Calendar();
+			carCalendar.setDescription("Test coliss !");
+			carCalendar.setSummary("Un sommaire esti !");
+			
+			System.out.println(calendar.calendars().insert(carCalendar).execute());	
 		} catch (Exception ex) {
 			throw new RuntimeException(ex);
 		}
 	}
+	
 }
