86b1eaa92605d8b0f2392af0fa7ee09684924f42
root <root@ns3363715.ip-91-121-3.eu>
2014-09-23 22:49:58 +0200

    Fixed mysql deployment script

--- docker-resources/scripts/mysql/deploy.sh
+++ docker-resources/scripts/mysql/deploy.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
 NAME="wevan-mysql"
 RUNTIME_OPTS="-d"
 PORT=3306
 IMAGE="tutum/mysql"
+PASSWORD="admin"
 
-CMD="docker run --name $NAME -p $PORT:PORT $RUNTIME_OPTS $IMAGE"
+CMD="docker run --name $NAME -p $PORT:$PORT -e MYSQL_PASS="$PASSWORD" $RUNTIME_OPTS $IMAGE"
 
 echo Deploting MySql with docker command :
-echo "\t$CMD"
+echo "	$CMD"
 
-bash $CMD
+$CMD
 
 echo "MySql container has been created. Proceed with reset of admin account."
