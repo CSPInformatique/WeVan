69af565b3e669a780e40d7aca7a6a16a3a841d99
root <root@ns3363715.ip-91-121-3.eu>
2014-09-23 23:25:05 +0200

    Docker deployment fixes

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 mvn install:install-file -Dfile=lib/amaranth-1.0.0.jar -DgroupId=com.cspinformatique -DartifactId=amaranth -Dversion=1.0.0 -Dpackaging=jar
 
-mvn install:install-file -Dfile=lib/jasperreports-fonts-5.5.1.jar -DgroupId=com.cspinformatique -DartifactId=jasperreports-fonts -Dversion=5.5.1 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/jasperreports-fonts-5.5.1.jar -DgroupId=net.sf.jasperreports -DartifactId=jasperreports-fonts -Dversion=5.5.1 -Dpackaging=jar
 
-mvn install:install-file -Dfile=lib/jasperreports-fonts-5.5.1.jar -DgroupIdnet.sf.jasperreports -DartifactId=jasperreports-functions -Dversion=5.5.1 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/jasperreports-functions-5.5.1.jar -DgroupId=net.sf.jasperreports -DartifactId=jasperreports-functions -Dversion=5.5.1 -Dpackaging=jar
+
+mvn clean install
