5cdcef8ce9933ec72af30d0846c2d3198901cd53
root <root@ns3363715.ip-91-121-3.eu>
2014-09-23 23:35:11 +0200

    Docker fixes

--- docker-resources/image/docker-build.sh
+++ docker-resources/image/docker-build.sh
@@ -1,2 +1,2 @@
-unzip -o ../target/docker.zip -d ../target
-docker build -t cspinformatique/wevan ../target/docker
+unzip -o ../../target/docker.zip -d ../../target
+docker build -t cspinformatique/wevan ../../target/docker
