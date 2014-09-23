4f29446b8afc40b8f60b254cb53206f880d10f5e
dlavoie <dlavoie@cspinformatique.com>
2014-09-23 23:50:00 +0200

    Fixed entity table mapping.

--- src/main/java/com/cspinformatique/wevan/entity/ElixirAudit.java
+++ src/main/java/com/cspinformatique/wevan/entity/ElixirAudit.java
@@ -4,8 +4,10 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity(name="elixiraudit")
+@Entity
+@Table(name="elixiraudit")
 public class ElixirAudit {
 	private Long reservationId;
 	private long contractId;
