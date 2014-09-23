04abe41700e0b543bae125bbce0340cd6c5aabb6
dlavoie <dlavoie@cspinformatique.com>
2014-09-23 22:55:17 +0200

    Dockerize Wevan

---
+++ docker-resources/image/docker-build.sh
@@ -0,0 +1,2 @@
+unzip -o ../target/docker.zip -d ../target
+docker build -t cspinformatique/wevan ../target/docker
---
+++ docker-resources/scripts/wevan/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+NAME="wevan-test"
+PORT=8050
+IMAGE="cspinformatiqu/wevan"
+RUNTIME_OPTS="-d"
+
+docker run --name $NAME -p $8050:$8050 $RUNTIME_OPTS $IMAGE
