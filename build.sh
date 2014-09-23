e3848a6be84421229c44086ae92bda5ac62be6ca
dlavoie <dlavoie@cspinformatique.com>
2014-09-23 23:10:05 +0200

    Added jaspersofts missing libs and build scripts.

---
+++ build.sh
@@ -0,0 +1,5 @@
+mvn install:install-file -Dfile=lib/amaranth-1.0.0.jar -DgroupId=com.cspinformatique -DartifactId=amaranth -Dversion=1.0.0 -Dpackaging=jar
+
+mvn install:install-file -Dfile=lib/jasperreports-fonts-5.5.1.jar -DgroupId=com.cspinformatique -DartifactId=jasperreports-fonts -Dversion=5.5.1 -Dpackaging=jar
+
+mvn install:install-file -Dfile=lib/jasperreports-fonts-5.5.1.jar -DgroupIdnet.sf.jasperreports -DartifactId=jasperreports-functions -Dversion=5.5.1 -Dpackaging=jar
